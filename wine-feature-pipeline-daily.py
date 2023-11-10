3c1e268c446ce0f73a4a08d2d36224410a861922
marie <mariegotthardt224@gmail.com>
2023-11-10 20:29:47 +0100

    structure for wine-feature-pipeline-daily.py

--- wine-feature-pipeline-daily.py
+++ wine-feature-pipeline-daily.py
@@ -1,9 +1,28 @@
 import os
 import modal
 
-LOCAL=True
+def generate_wine():
+    """
+    Returns a new wine as a single row in a DataFrame
+    """
+    import pandas as pd
+    import random
+      # TODO
 
-if LOCAL == False:
-   stub = modal.Stub("wine_daily")
-   image = modal.Image.debian_slim().pip_install(["hopsworks"])
+    #return wine_df
 
+
+def g():
+    import hopsworks
+    import pandas as pd
+
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+
+    wine_df = generate_wine()
+
+    wine_fg = fs.get_feature_group(name="wine",version=1)
+    wine_fg.insert(wine_df)
+
+if __name__ == "__main__":
+   g()
